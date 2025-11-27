4acd57f46ee2346684875a4e5e931c28d95d418a
Joodarbeshim05 <162398776+Joodarbeshim05@users.noreply.github.com>
2025-11-27 23:42:35 +0600

    updated home page

--- front/components/pages/HomePage.tsx
+++ front/components/pages/HomePage.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useEffect } from "react";
 import Categories from "./home/Categories";
 import Communications from "./home/Communications";
 import Popular from "./home/Popular";
@@ -5,6 +7,9 @@ import Share from "./home/Share";
 import Welcome from "./home/Welcome";
 
 const HomePage = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <>
       <Welcome />
